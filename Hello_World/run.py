6d2a2292803b1a0859b38dc50d597950b2a58439
GulsahKose <gulsah.1004@gmail.com>
2014-06-23 00:37:46 +0300

    This code was simplified

--- Hello_World/run.py
+++ Hello_World/run.py
@@ -1,3 +1,11 @@
 #!flask/bin/python
-from app import app
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
+
 app.run(debug = True)
