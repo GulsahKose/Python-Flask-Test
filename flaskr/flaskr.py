52b7f8ad97c55f37a3074770e715ad5e6d5a53f6
GulsahKose <gulsah.1004@gmail.com>
2014-07-01 12:10:55 +0300

    Added handler for 404 not found

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -95,6 +95,9 @@ def logout():
 	flash('You were logged out')
 	return redirect(url_for('show_entries'))
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
 
 if __name__ == '__main__':
 	app.run()
